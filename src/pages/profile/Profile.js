2b32177d586937aef5e68ebe72f8c9836d58a6b3
rahimatunnimah <rahimatunnimah@gmail.com>
2022-09-12 22:16:48 +0700

    revisi profile page

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,24 +1,44 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./Profile.css";
-import imageUser from "../../assets/profile.png";
 import MyRecipe from "../../components/recipe/MyRecipe";
+import axios from "axios";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
+  const [profile, setProfile] = useState([]);
+  const { auth } = useSelector((state) => state);
+  const { user } = auth;
+
   useEffect(() => {
-    if (!localStorage.getItem("token")) {
-      window.location.href = "/";
-    }
+    getProfile();
   }, []);
+
+  const config = {
+    headers: {
+      Authorization: `Bearer ${auth?.token}`,
+    },
+  };
+
+  const getProfile = () => {
+    axios
+      .get(`${process.env.REACT_APP_URL}users/${user?.id}`, config)
+      .then((res) => {
+        setProfile(res?.data?.data[0]);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
   return (
     <>
       <div className="row mt-5">
         <div className="col-md-12 mt-5 profile text-center">
-          <img className="img-user-profile" src={imageUser} alt="Avatar" />
+          <img className="img-user-profile" src={profile.image} alt="Avatar" />
         </div>
       </div>
       <div className="row ">
         <div className="col-md-12 mt-3 profile text-center">
-          <h5>Rahimatun Ni'mah</h5>
+          <h5>{profile.username}</h5>
         </div>
       </div>
       <MyRecipe />
